d18c9f349067b17ff01900fbc9ddc87ecb590c02
Paul <50247665+Jester0027@users.noreply.github.com>
2023-12-16 13:15:44 +0100

    feat: Parse and sanitize markdown content (#3)

--- app/Listeners/DiscordSocket.ts
+++ app/Listeners/DiscordSocket.ts
@@ -1,20 +1,41 @@
 import type { EventsList } from '@ioc:Adonis/Core/Event'
 import Logger from '@ioc:Adonis/Core/Logger'
+import { marked } from 'marked'
+import createDOMPurify from 'dompurify'
+import { JSDOM } from 'jsdom'
 
 export default class DiscordSocket {
   public async onReady([]: EventsList['discord:ready']) {
     Logger.info('Discord bot ready')
   }
 
-  public async onThreadCreated([]: EventsList['discord:thread:created']) {
+  // @ts-ignore
+  public async onThreadCreated([thread, newThread]: EventsList['discord:thread:created']) {
+    // const initialMessage = await thread.fetchStarterMessage()
+    // const html = await this.#parseMarkdown(initialMessage?.content)
     Logger.info('Thread created')
   }
 
-  public async onThreadUpdated([]: EventsList['discord:thread:updated']) {
+  // @ts-ignore
+  public async onThreadUpdated([thread]: EventsList['discord:thread:updated']) {
+    // const initialMessage = await thread.fetchStarterMessage()
     Logger.info('Thread updated')
   }
 
-  public async onThreadDeleted([]: EventsList['discord:thread:deleted']) {
+  // @ts-ignore
+  public async onThreadDeleted([thread]: EventsList['discord:thread:deleted']) {
+    // const initialMessage = await thread.fetchStarterMessage()
     Logger.info('Thread deleted')
   }
+
+  #sanitize(content: string | null) {
+    if (content === null) return null
+    const window = new JSDOM('').window
+    const purify = createDOMPurify(window)
+    return purify.sanitize(content, { KEEP_CONTENT: true, SANITIZE_DOM: true })
+  }
+
+  // @ts-ignore
+  #parseMarkdown = async (content?: string) =>
+    this.#sanitize(content ? await marked.parse(content) : null)
 }
